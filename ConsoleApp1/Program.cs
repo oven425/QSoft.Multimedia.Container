902de7d2159d771e7b6268f367b27674377670b8
oven425 <oven425@yahoo.com.tw>
2023-01-03 22:45:10 +0800

    update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QSoft.Media.Container.MP4;
 
 namespace ConsoleApp1
 {
@@ -10,6 +11,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Mp4Reader reader = new Mp4Reader();
+            reader.Open("../../../sample-mp4-file-small (2).mp4");
         }
     }
 }
