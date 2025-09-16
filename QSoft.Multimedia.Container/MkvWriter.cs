c3fe4a874948687f211d69ebcc581311b1ea9781
oven425 <oven425@yahoo.com.tw>
2025-09-16 19:42:39 +0800

    update
    1. test mkv r/w

--- QSoft.Multimedia.Container/MkvWriter.cs
+++ QSoft.Multimedia.Container/MkvWriter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,7 @@ namespace QSoft.Multimedia.Container
         public void Open()
         {
             WritetEBML_ID(0x1a45dfa3);
+            WriteEBML_Size(25);
         }
 
         void WritetEBML_ID(int data)
@@ -33,27 +35,44 @@ namespace QSoft.Multimedia.Container
 
         void WriteEBML_Size(int data)
         {
-            byte first = (byte)stream.ReadByte();
-            int length = 1;
-            byte mask = 0x80;
-            while ((first & mask) == 0 && mask != 0)
+            var bitc = data switch
             {
-                mask >>= 1;
-                length++;
-            }
+                <= 127 => 0x80,
+                <= 16383 => 0x40,
+                <= 2097151 => 0x20,
+                <= 268435455 => 0x10,
+                _ => 0
+            };
 
-            if (length > 8)
-                throw new InvalidOperationException("Invalid EBML size encoding");
-            int value = (int)(first & (mask - 1));
-            if (length > 1)
-            {
-                Span<byte> buffer = stackalloc byte[length - 1];
-                stream.Read(buffer);
-                for (int i = 0; i < length - 1; i++)
-                    value = (value << 8) | buffer[i];
-            }
-            //return value;
+
+            var aaa = bitc | data;
+            var bbs = BitConverter.GetBytes(aaa);
+            Array.Reverse(bbs);
+            stream.Write(bbs,0, bbs.Length);
+
         }
 
+
+        //VINT 長度 有效位元數 最大值
+        //1 byte	7 bits	127
+        //2 bytes	14 bits	16,383
+        //3 bytes	21 bits	2,097,151
+        //4 bytes	28 bits	268,435,455
+        //5 bytes	35 bits	34,359,738,367
+        //6 bytes	42 bits	4,398,046,511,103
+        //7 bytes	49 bits	562,949,953,421,311
+        //8 bytes	56 bits	72,057,594,037,927,935
+
+        //By Copilot
+        int GetEbmlVintLength(ulong value)
+        {
+            for (int length = 1; length <= 8; length++)
+            {
+                ulong maxValue = (1UL << (7 * length)) - 1;
+                if (value <= maxValue)
+                    return length;
+            }
+            throw new ArgumentOutOfRangeException(nameof(value), "Value too large for EBML VINT");
+        }
     }
 }
